04c6b630cdb5eb4731341b325c5e5e1c7da55878
amir <amirghassempour@protonmail.com>
2022-04-23 18:45:40 +0430

    webdevfun - basic math

---
+++ webdevfun/javascript.js
@@ -0,0 +1,11 @@
+const chp = 5; // children ticket price.
+const adp = 12; // adults ticket price.
+
+let children = 2; // number of children.
+let adults = 3; // number of adults.
+
+let total; // total price.
+
+total = adults*12 + children*5; // calculate total price.
+
+console.log(total); // output the price to the console.
