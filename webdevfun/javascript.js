9696c630086854cc84570639b618e1945312c026
amir <amirghassempour@protonmail.com>
2022-04-24 04:23:42 +0430

    webdevfun - event handling

--- webdevfun/javascript.js
+++ webdevfun/javascript.js
@@ -8,11 +8,11 @@ if (children < 0) {
     children = 0;
 }
 
-function calc() {
-	children*5 + adults*12;
+function calc(c, a) {
+	c*5 + a*12;
 }
 
-var price = calc();
+var price = calc(children, adults);
 console.log(price)
 
 for (let i=1; i<=adults; i++) {
@@ -20,4 +20,14 @@ for (let i=1; i<=adults; i++) {
 }
 for (let i=1; i<=children; i++) {
     console.log("child Ticket #" + i)
+}
+
+window.onload = function() {
+    let btn = document.getElementById("buybtn");
+    btn.onclick = function() {
+        let adults = document.getElementById("adults").value;
+        let children = document.getElementById("children").value;
+        let price = calc(children, adults);
+        alert(price)
+    }
 }
