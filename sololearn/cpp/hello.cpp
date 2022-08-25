7d811014312d83c182db5512af442cb6fe4ec777
amir <amirghassempour@protonmail.com>
2022-08-25 11:21:54 +0000

    add note to cpp/hello.cpp

--- sololearn/cpp/hello.cpp
+++ sololearn/cpp/hello.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() {
     //print "C++ is cool"
-    cout << "C++ is cool";
-    
+    cout << "C++ is cool" << endl;
+    cout << "Note: I had to add -lstdc++ to gcc" << endl;
     return 0;
 }
